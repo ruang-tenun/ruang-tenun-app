de28c4b17a4f08a458b455f2e4bcbcbc4af2c9d0
dias1202 <diasdwig@gmail.com>
2024-11-24 14:18:55 +0700

    RTAB4:Create Authentication Login And Register UI with navigation

--- app/src/main/java/com/ruangtenun/app/authentication/register/RegisterFragment.kt
+++ app/src/main/java/com/ruangtenun/app/authentication/register/RegisterFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.ruangtenun.app.R
+import com.ruangtenun.app.authentication.login.LoginFragment
 
 class RegisterFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,24 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        view.findViewById<Button>(R.id.back_button).setOnClickListener {
+            moveToLogin()
+        }
+
+        view.findViewById<TextView>(R.id.tv_sign_in_click).setOnClickListener {
+            moveToLogin()
+        }
+        return view
+    }
+
+    private fun moveToLogin() {
+        parentFragmentManager.beginTransaction()
+            .replace(
+                R.id.fragment_container,
+                LoginFragment(),
+                LoginFragment::class.java.simpleName)
+            .commit()
     }
 }
